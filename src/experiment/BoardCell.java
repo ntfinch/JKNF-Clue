56a980ca880b5930c41fcb742967ace4a87a7979
Ian Cairns <iancairns@Ians-MacBook-Pro.local>
2017-02-24 12:11:15 -0700

    BoardCell constructor removed

--- src/experiment/BoardCell.java
+++ src/experiment/BoardCell.java
@@ -2,12 +2,4 @@ package experiment;
 
 public class BoardCell {
 	private int row, column;
-	
-	public BoardCell(int row, int column) {
-		super();
-		this.row = row;
-		this.column = column;
-	}
-
-	
 }
