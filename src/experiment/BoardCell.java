9f7e5f71132b4e86bd2e5784ffca2530e9021c36
Ian Cairns <iancairns@Ians-MacBook-Pro.local>
2017-02-26 12:31:04 -0700

    testAdjacency pass

--- src/experiment/BoardCell.java
+++ src/experiment/BoardCell.java
@@ -1,5 +1,12 @@
 package experiment;
 
 public class BoardCell {
-	private int row, column;
+	public int row, column;
+
+	public BoardCell(int row, int column) {
+		super();
+		this.row = row;
+		this.column = column;
+	}
+	
 }
--- src/experiment/IntBoard.java
+++ src/experiment/IntBoard.java
@@ -19,11 +19,51 @@ public class IntBoard {
 		visited = new HashSet<BoardCell>();
 		targets = new HashSet<BoardCell>();
 		grid = new BoardCell[boardSizeX][boardSizeY];
+		for(int x = 0;x<boardSizeX; x++){
+			for(int y = 0; y<boardSizeY;y++){
+				grid[x][y]=new BoardCell(x,y);
+			}
+		}
 		calcAdjacencies(); //setup adjMtx
 		
 	}
 	
 	private void calcAdjacencies(){
+		BoardCell boardKey;
+		BoardCell boardValue;
+		for(int x = 0; x<grid.length;x++){
+			for(int y = 0; y<grid[0].length;y++){
+				boardKey = grid[x][y];
+				if(x-1>=0){
+					boardValue = grid[x-1][y];
+					addToAdjMap(boardKey, boardValue);
+				}
+				if(y-1>=0){
+					boardValue = grid[x][y-1];
+					addToAdjMap(boardKey, boardValue);
+				}
+				if(x+1<grid.length){
+					boardValue = grid[x+1][y];
+					addToAdjMap(boardKey, boardValue);
+				}
+				if(y+1<grid[0].length){
+					boardValue = grid[x][y+1];
+					addToAdjMap(boardKey, boardValue);
+				}
+			}
+		}
+		
+		
+	}
+	
+	private void addToAdjMap(BoardCell key, BoardCell value){
+		if(adjMtx.containsKey(key))
+			adjMtx.get(key).add(value);
+		else{
+			Set<BoardCell> temp = new HashSet<BoardCell>(); 
+			temp.add(value);
+			adjMtx.put(key, temp);
+		}
 		
 	}
 	
@@ -35,10 +75,10 @@ public class IntBoard {
 		return null;
 	}
 	public Set<BoardCell> getAdjList(BoardCell cell){
-		return null;
+		return adjMtx.get(cell);
 	}
 	
 	public BoardCell getCell(int x, int y){
-		return null;
+		return grid[x][y];
 	}
 }
