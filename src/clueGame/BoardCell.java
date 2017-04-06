c0cbb43b676a61229a106f2cd7cffbcf53bfbed2
Jordan King <jordanking@mymail.mines.edu>
2017-04-06 17:23:00 -0600

    Drawing board cell methods almost fully completed

--- src/clueGame/BoardCell.java
+++ src/clueGame/BoardCell.java
@@ -4,7 +4,7 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 
-public class BoardCell implements Comparable<BoardCell>{
+public class BoardCell implements Comparable<BoardCell> {
 	private int row, column;
 	private char initial = ' ';
 	private boolean isDoorway = false;
@@ -20,12 +20,13 @@ public class BoardCell implements Comparable<BoardCell>{
 		this.row = row;
 		this.column = column;
 		this.initial = myChar;
-		if (myChar == 'W'){
+		if (myChar == 'W') {
 			isWalkway = true;
-		}
-		else if (myChar != 'X')
+		} else if (myChar != 'X')
 			isRoom = true;
-		
+		xPos = column * CELL_SIZE;
+		yPos = row * CELL_SIZE;
+
 	}
 
 	public boolean isDoorway() {
@@ -39,26 +40,48 @@ public class BoardCell implements Comparable<BoardCell>{
 		}
 	}
 
-	public void drawBoardCells(Graphics g){
-		if(this.isWalkway)
+	public void drawBoardCells(Graphics g) {
+		if (this.isWalkway)
 			g.setColor(Color.GRAY);
 		else
 			g.setColor(blu);
-		if(this.isDoorway)
-			drawDoorway(g);
-		 g.fillRect(this.row, this.column, PIECE_SIZE, PIECE_SIZE);
-		    if ((isWalkway()) || (this.highlighted))
-		    {
-		      g.setColor(Color.black);
-		      g.drawRect(this.x, this.y, PIECE_SIZE, PIECE_SIZE);
-		    }
-	}
-	
-	public void drawDoorway(Graphics g)
-	  {
 		
-	  }
-	
+		if (this.isDoorway)
+			drawDoorway(g);
+		
+		//Draw individual cells
+		g.fillRect(this.xPos, this.yPos, CELL_SIZE, CELL_SIZE);
+		
+		//Draw borders
+		if ((isWalkway())) {
+			g.setColor(Color.BLACK);
+			g.drawRect(this.xPos, this.yPos, CELL_SIZE, CELL_SIZE);
+		}
+	}
+
+	public void drawDoorway(Graphics g) {
+		g.setColor(Color.YELLOW);
+		int sideSize = CELL_SIZE;
+		if (this.doorDirection == DoorDirection.LEFT) {
+			for (int i = 0; i < 4; i++) {
+				g.drawLine(this.xPos + i, this.yPos, this.xPos + i, this.yPos + sideSize);
+			}
+		} else if (this.doorDirection == DoorDirection.RIGHT) {
+			for (int i = 0; i < 4; i++) {
+				g.drawLine(this.xPos + sideSize - i, this.yPos, this.xPos + sideSize - i, this.yPos + sideSize);
+			}
+		} else if (this.doorDirection == DoorDirection.UP) {
+			for (int i = 0; i < 4; i++) {
+				g.drawLine(this.xPos, this.yPos + i, this.xPos + sideSize, this.yPos + i);
+			}
+		} else {
+			for (int i = 0; i < 4; i++) {
+				g.drawLine(this.xPos, this.yPos + sideSize - i, this.xPos + sideSize, this.yPos + sideSize - i);
+			}
+		}
+
+	}
+
 	public boolean isWalkway() {
 		return isWalkway;
 	}
