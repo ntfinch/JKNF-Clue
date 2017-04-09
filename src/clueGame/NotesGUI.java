22db80a191da08b690a9037c2f729962a2e8d502
Noah Finch <Noah Finch>
2017-04-08 18:08:44 -0600

    Added all the options for the detective notes, Still need to position them correctly

--- src/clueGame/NotesGUI.java
+++ src/clueGame/NotesGUI.java
@@ -9,6 +9,7 @@ import java.util.Map;
 import java.util.Scanner;
 
 import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -26,6 +27,10 @@ public class NotesGUI extends JPanel{
 		setLayout(new GridLayout(3, 2));
 		add(peopleIndicator());
 		add(roomIndicator());
+		add(weaponIndicator());
+		//add(bestPerson());
+		//add(bestRoom());
+		//add(bestWeapon());
 	}
 
 	
@@ -45,7 +50,7 @@ public class NotesGUI extends JPanel{
 	
 	private Component peopleIndicator() throws FileNotFoundException {
 		JPanel people = new JPanel();
-		people.setLayout(new GridLayout(3,1));
+		people.setLayout(new GridLayout(3,2));
 		people.add(createLabel("People"), BorderLayout.NORTH);
 		
 		File person = new File("Person.txt");
@@ -58,13 +63,29 @@ public class NotesGUI extends JPanel{
 
 		return people;
 	}
-	
+	private Component bestPerson() throws FileNotFoundException {
+		JPanel menu = new JPanel();
+		//menu.setLayout(new GridLayout(3,1));
+		menu.add(createLabel("Person Guess"), BorderLayout.NORTH);
+		JComboBox personList = new JComboBox();
+		menu.add(personList);
+		
+		File person = new File("Person.txt");
+		Scanner personReader = new Scanner(person);
+		while (personReader.hasNextLine()) {
+			personList.addItem(personReader.nextLine());
+		}
+		personList.addItem("Unsure");
+		personReader.close();
+		
+		return menu;
+	}
 	
 	private Component roomIndicator() throws FileNotFoundException {
 		Map<Character, String> legendMap = Board.getInstance().getLegendMap();
 		JPanel rooms = new JPanel();
-		rooms.setLayout(new GridLayout(3,1));
-		rooms.add(createLabel("People"), BorderLayout.CENTER);
+		rooms.setLayout(new GridLayout(3,2));
+		rooms.add(createLabel("Rooms"), BorderLayout.CENTER);
 		
 		for (String room : legendMap.values()) {
 			if (!room.equals("Walkway") && !room.equals("Closet")) {
@@ -72,6 +93,54 @@ public class NotesGUI extends JPanel{
 			}
 		}
 		return rooms;
+	}
+	private Component bestRoom() throws FileNotFoundException {
+		Map<Character, String> legendMap = Board.getInstance().getLegendMap();
+		JPanel menu = new JPanel();
+		//menu.setLayout(new GridLayout(3,1));
+		menu.add(createLabel("Room Guess"), BorderLayout.CENTER);
+		JComboBox roomList = new JComboBox();
+		menu.add(roomList);
+		
+		for (String room : legendMap.values()) {
+			if (!room.equals("Walkway") && !room.equals("Closet")) {
+			roomList.addItem(room);
+			}
+		}
+		roomList.addItem("Unsure");
+		return menu;
+	}
+	
+	private Component weaponIndicator() throws FileNotFoundException {
+		JPanel weapon = new JPanel();
+		weapon.setLayout(new GridLayout(3,2));
+		weapon.add(createLabel("Weapons"), BorderLayout.SOUTH);
+		
+		File weapons = new File("weapon.txt");
+		Scanner weaponReader = new Scanner(weapons);
+		while (weaponReader.hasNextLine()) {
+			weapon.add(createCheckBox(weaponReader.nextLine()));
+		}
+		weaponReader.close();
+		
+
+		return weapon;
+	}
+	private Component bestWeapon() throws FileNotFoundException {
+		JPanel menu = new JPanel();
+		//menu.setLayout(new GridLayout(3,1));
+		menu.add(createLabel("Weapon Guess"), BorderLayout.SOUTH);
+		JComboBox weaponList = new JComboBox();
+		menu.add(weaponList);
+		
+		File weapon = new File("weapon.txt");
+		Scanner weaponReader = new Scanner(weapon);
+		while (weaponReader.hasNextLine()) {
+			weaponList.addItem(weaponReader.nextLine());
+		}
+		weaponReader.close();
+		weaponList.addItem("Unsure");
+		return menu;
 	}
 
 	
