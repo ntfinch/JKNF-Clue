b3d33065479104b209b797f8996a9e762c13f24e
Noah Finch <ntfinch@mines.edu>
2017-04-18 17:01:48 -0600

    Made the suggestion class create all needed lables, buttons and dropdown menus. TODO: add current player room.

--- src/clueGame/SuggestionGUI.java
+++ src/clueGame/SuggestionGUI.java
@@ -2,29 +2,53 @@ package clueGame;
 
 import java.awt.Component;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+import javax.swing.JButton;
 import javax.swing.JComboBox;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.TitledBorder;
 
+
 public class SuggestionGUI extends JPanel {
 public Board board;
+JButton submit, cancel;
 
 
-public SuggestionGUI() {
+
+public SuggestionGUI() throws FileNotFoundException {
 	this.board = Board.getInstance();
 	setLayout (new GridLayout(4,2));
-	
+	add(createLabel("Your Room"));
+	//add(roomGuess(room));
+	add(createLabel("Person"));
+	add(personGuess());
+	add(createLabel("Weapon"));
+	add(bestWeapon());
+	add(submitSuggestion());
+	add(cancelSuggestion());
 }
 
-private Component currentRoom() {
+private Component createLabel(String text) {
+	JLabel label = new JLabel();
+	label.setText(text);
+	return label;
+}
+
+
+
+private Component roomGuess(BoardCell room) {
+	String currentRoom = Board.getInstance().getRoomName(room.getInitial());
+	JPanel menu = new JPanel();
+	menu.add(createLabel(currentRoom));
 	return board;
 	
 }
-
 private Component personGuess() throws FileNotFoundException {
 	JPanel menu = new JPanel();
 	JComboBox personList = new JComboBox();
@@ -40,7 +64,6 @@ private Component personGuess() throws FileNotFoundException {
 	personReader.close();
 	return menu;
 }
-
 private Component bestWeapon() throws FileNotFoundException {
 	JPanel menu = new JPanel();
 	menu.setBorder(new TitledBorder("Weapon Guess"));
@@ -56,6 +79,39 @@ private Component bestWeapon() throws FileNotFoundException {
 	return menu;
 }
 
+ButtonActions listener = new ButtonActions();
+private class ButtonActions implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == SuggestionGUI.this.submit)
+			SuggestionGUI.this.makeSuggestionHit();
+		if(e.getSource() == SuggestionGUI.this.cancel)
+			SuggestionGUI.this.cancelHit();
+	}
+	
+}
+
+private JButton submitSuggestion() {
+	submit = new JButton("Submit");
+	this.submit.addActionListener(listener);
+	return submit;
+	
+}
+private JButton cancelSuggestion() {
+	cancel = new JButton("Submit");
+	this.cancel.addActionListener(listener);
+	return cancel;
+	
+}
+
+//TODO
+public void makeSuggestionHit() {
+	
+}
+public void cancelHit() {
+	
+}
 
 
 }
